4b19478b4e4c762ed9fd98cae1759c1afc58da13
Megan <megantech@hotmail.com>
2023-07-20 14:20:09 +0000

    Navbar using bootstrap added

---
+++ my-app/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <div className="container">
+        <Navbar.Brand href="/">Your Brand</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
+  );
+};
+
+export default NavBar;
